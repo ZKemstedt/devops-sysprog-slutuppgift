115d0f3037c55cb3a6fc6c8bf46dd3be466eb871
Zephyro Kemstedt <zkemstedt@gmail.com>
2020-09-21 13:24:51 +0200

    initial commit

---
+++ __main__.py
@@ -0,0 +1,45 @@
+
+class BoardGame(object):
+
+    def __init__(self):
+        pass
+        # self.title
+        # self.no_players
+        # self.duration
+        # self.age_recommendation
+
+
+class Collection(object):
+
+    def __init__(self):
+        pass
+        # self.boardgames
+        # self.name  # name to differentiate between different collections, default name = 'default' ?
+
+# -- Data --
+
+# - load -
+# file -> Collection [ BoardGames ]
+
+# - save -
+# Collection [ BoardGames ] -> file
+
+
+# -- Input / Shell ? --
+
+# - Basic -
+# list all boardgames (in collection)
+# create new boardgame
+# select boardgame
+# edit existing boardgame
+# remove boardgame
+
+# - Advanced -
+# search (filter) collection of boardgames
+
+# - Bonus -
+# list all boardgames (in all collections)
+# change collection
+# played
+# user_ratings
+# filter-extension: show non-exact matches at bottom of list.
