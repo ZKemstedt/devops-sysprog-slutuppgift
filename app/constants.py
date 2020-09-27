c70058558306b60283fd089aeb6136f75bb17653
Zephyro Kemstedt <zkemstedt@gmail.com>
2020-09-27 19:46:52 +0200

    merge utils.py into constants.py

--- app/constants.py
+++ app/constants.py
@@ -1,4 +1,8 @@
-CLEAR = 50 * '\n'
+class Whitespace(object):
+    line = '\n' + '---' * 30 + '\n'
+    weak_line = '\n' + '-  ' * 30 + '\n'
+    clear = 50 * '\n'
+
 
 INTRODUCTION = """
     Boardgame Collection Manager
--- app/utils.py
+++
@@ -1,5 +0,0 @@
-
-class Whitespace(object):
-    line = '\n' + '---' * 30 + '\n'
-    weak_line = '\n' + '-  ' * 30 + '\n'
-    clear = 50 * '\n'
