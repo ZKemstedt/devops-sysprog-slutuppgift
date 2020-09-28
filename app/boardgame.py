4758879bebbefea83b7a69fc379683c3641925a6
Zephyro Kemstedt <zkemstedt@gmail.com>
2020-09-28 10:45:15 +0200

    refactor age_recommendation -> recommended_age, format boardgame __init__, refactor modify -> _modify, add flake8 exclusions to field_checks

--- app/boardgame.py
+++ app/boardgame.py
@@ -1,40 +1,55 @@
-from typing import List
+from typing import List, Dict, Callable
 
-# how to perform check? eg: is the value '5' ok for field (key) 'duration' ?
-# check_result = BOARDGAME_FIELD_CHECK['duration']('5')
-# -> True
-BOARDGAME_FIELD_CHECK = {
-    'title':                lambda x: not x.isdigit(),
-    'players':              lambda x: x.isdigit(),
-    'duration':             lambda x: x.isdigit(),
-    'age_recommendation':   lambda x: x.isdigit(),
-    'times_played':         lambda x: x.isdigit(),
-    'rating':               lambda x: x.isdigit(),
+from app.utils import str_sized
+
+
+# -- how to use field checks --
+# if field in FIELD_CHECK and FIELD_CHECK[field](value):
+#   do stuff...
+# eg: try to set field 'name' to 'test'
+# if field in FIELD_CHECK and FIELD_CHECK['name']('test'):
+#   item._modify('name', 'test')
+
+BOARDGAME_FIELD_CHECK: Dict[str, Callable[[str], bool]] = {
+    'title':                lambda x: not x.isdigit(),          # noqa 
+    'players':              lambda x: x.isdigit(),              # noqa 
+    'duration':             lambda x: x.isdigit(),              # noqa 
+    'recommended_age':      lambda x: x.isdigit(),              # noqa 
+    'times_played':         lambda x: x.isdigit(),              # noqa 
+    'rating':               lambda x: x.isdigit(),              # noqa 
     }
 
 
 class BoardGame(object):
 
-    def __init__(self, title: str, players: str, duration: str, age: str, times_played: str = '0', rating: str = ''):
+    def __init__(self,
+                 title: str,
+                 players: str,
+                 duration: str,
+                 recommended_age: str,
+                 times_played: str = '0',
+                 rating: str = ''):
+
         self.title = title
         self.players = players
         self.duration = duration
-        self.age_recommendation = age
+        self.recommended_age = recommended_age
         self.times_played = times_played
         self.rating = rating
-        self.name = self.title
+        self.name = self.title  # just an alias
 
-    def modify(self, field: str, value: str) -> None:
+    def _modify(self, field: str, value: str) -> None:
         setattr(self, field, value)
 
     def save(self) -> List:
-        return [self.title, self.players, self.duration, self.age_recommendation, self.rating, self.times_played]
+        return [self.title, self.players, self.duration, self.recommended_age, self.rating, self.times_played]
 
     def __str__(self) -> str:
-        return (str_sized(self.title, 29).ljust(30)
+        return (''
+                + str_sized(self.title, 29).ljust(30)
                 + str_sized(self.players, 4).rjust(5)
                 + (str_sized(self.duration, 8) + 'm ').rjust(10)
-                + str_sized(self.age_recommendation, 16).rjust(17)
+                + str_sized(self.recommended_age, 16).rjust(17)
                 + str_sized(self.times_played, 13).rjust(14)
                 + str_sized(self.rating, 7).rjust(8)
                 )
@@ -42,16 +57,3 @@ class BoardGame(object):
     def inc_times_played(self, inc: int = 1) -> None:
         # I like storing my integers as strings, okay!?
         self.times_played = f'{int(self.times_played) + inc}'
-
-
-def str_sized(string: str, lenth: int) -> str:
-    """Force a string to be max `lenth` characters long by cutting off any extra characters.
-
-    Args:
-        string (str): the string to format
-        lenth (int): the max lenth of the string
-
-    Returns:
-        str: the lenth-formatted string
-    """
-    return string if len(string) <= lenth else string[:lenth] + ' '
